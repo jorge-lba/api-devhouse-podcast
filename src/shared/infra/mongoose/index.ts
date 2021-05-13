2903d33f7701de02bf2c84cf3847d2158d3b68dc
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-05-12 22:17:46 -0300

    Add - Parameter by config mongoose

--- src/shared/infra/mongoose/index.ts
+++ src/shared/infra/mongoose/index.ts
@@ -1,5 +1,5 @@
 import { connect } from "mongoose"
 
-const Connection = () => connect( `${process.env.MONGO_DB_LOCAL_URL}`, {useNewUrlParser: true})
+const Connection = () => connect( `${process.env.MONGO_DB_LOCAL_URL}`, {useNewUrlParser: true, useUnifiedTopology: true })
 
 export { Connection }
